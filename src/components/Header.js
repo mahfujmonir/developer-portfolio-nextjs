151de9c398a962a705fe6bf1fc0f8967acfb425e
mahfujmonir <mdmahfujmonir@gmail.com>
2026-02-13 22:08:32 +0600

    Create Header.js with a React header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Mahfuj Monir</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
